40b9a4c2b2a0583511517da74561ba0ab06510de
hailizia <gobio1017@outlook.com>
2025-12-31 14:50:15 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,12 +2,12 @@
 
 # 标的选择
 
-backpack_ticker = 'BNB'
-backpack_public_key = '<替换成自己的Key>'
-backpack_secret_key = '<替换自己的Secret>'
+backpack_ticker = 'SOL'
+backpack_public_key = 'TBYytQ2WBVQ0SOGQkqThsFquhSuaYZWoiJVDWhUMQQA='
+backpack_secret_key = 'vL0ZSESjCgrCQIvbtWD+1Nn6ZeIFPF5vrmZNrXAVOIo='
 
 # 报价大小建议
 
 backpack_base_order_size_usd = 100
-backpack_risk_threshold = 0.5
-backpack_Q_max = 1.0
+backpack_risk_threshold = 5
+backpack_Q_max = 10
